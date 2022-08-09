a84010034d84ca31d2480c491feef620430a2693
ploy-thanaporn <tnpsp1501@gmail.com>
2022-08-09 16:16:45 +0700

    feat: edit , delete

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,5 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "./model";
 import "./style.css";
 import { AiFillEdit, AiFillDelete, AiOutlineCheckCircle } from "react-icons/ai";
@@ -11,17 +11,61 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  // edit ถูกใช้อยู่รึป่าว
+  const [edit, setEdit] = useState<boolean>(false);
+  // เก็บค่า edit ไว้เพื่อให้ระบุรายการที่จะการแก้ไข
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
   return (
-    <form className="todos-single">
-      <span className="todos-single-text">{todo.todo}</span>
+    <form className="todos-single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          value={editTodo}
+          className="todos-single-text"
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
+        <span className="todos-single-text">{todo.todo}</span>
+      ) : (
+        <span className="todos-single-text">{todo.todo}</span>
+      )}
+
       <div className="group-icon">
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <AiOutlineCheckCircle />
         </span>
       </div>
